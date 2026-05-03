ea973863502c19ab604fec9b1f1a2b5235c683f1
sarthaksingh94 <singhsarthak09@gmail.com>
2026-05-02 21:31:05 -0500

    Remenants of old pr

---
+++ src/inferstream/coordinator/__init__.py
@@ -0,0 +1,5 @@
+"""Coordinator module for InferStream.
+
+This module acts as the brain of the distributed system, handling the in-memory queue,
+assigning request IDs, and routing requests and responses to and from workers via gRPC.
+"""
